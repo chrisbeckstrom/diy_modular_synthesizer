ff6d8132bfc0b955df3b81f9a1b62bc6f36342d7
Chris Beckstrom <chrisbeckstrom@gmail.com>
2018-01-16 18:59:05 +0000

    created start script - connects everything automatically

--- pd_patches/rpi_zero/run.sh
+++ pd_patches/rpi_zero/run.sh
@@ -14,13 +14,18 @@
 # why?
 # so I can run a single script to get my pure data + arduino module running
 
+clear
+echo "#######################"
 echo "starting up Rpi+Arduino"
+echo "#######################"
 
 echo "killing existing ttymidi and pd sessions"
-sudo killall ttymidi && sudo killall pd
+sudo killall ttymidi
+sudo killall pd
 
 # full path to the pure data file
-patch='/home/pi/diy_modular_synthesizer/pd_patches/rpi_zero/rpi.pd'
+patch='/home/pi/diy_modular_synthesizer/pd_patches/rpi_zero/rpi.pd' # where is the pd file?
+options='-nogui -alsamidi -rt' # pure data command line options
 
 # which audio devices to use? (use `pd -nogui -listdev` to see what's available)
 # TODO: arrive at these numbers programmatically the way we do with midi devices below
@@ -35,7 +40,7 @@ midiout='1'
 arduinoName='mission_control'
 
 # where is the arduino?
-arduinoDev='/dev/ttyACM1' # i.e. '/dev/ttyACM0'
+arduinoDev='/dev/ttyACM0' # i.e. '/dev/ttyACM0'
 
 # create a tmux session for ttymidi (and use custom tmux configuration file)
 tmux -f /home/pi/.tmux/tmux.conf new -s ttymidi -d
@@ -48,6 +53,8 @@ tmux send-keys -t ttymidi "ttymidi -s $arduinoDev -v -n $arduinoName" ENTER
 # create a tmux session for pure data
 tmux -f /home/pi/.tmux/tmux.conf new -s puredata -d
 
+sleep 4 # wait for ttymidi to connect to alsa
+
 echo "pure data info:"
 echo "audio in dev: $audioin"
 echo "audioout dev: $audioout"
@@ -57,10 +64,10 @@ echo "patch: $patch"
 
 echo "--- starting pd --"
 
-# start the PD patch and keep it in the foreground
-tmux send-keys -t puredata "sudo pd -nogui -alsamidi -rt -audioindev $audioin -audiooutdev $audioout -midiindev $midiin -midioutdev $midiout $patch" ENTER
+# start the PD patch
+tmux send-keys -t puredata "sudo pd $options -audioindev $audioin -audiooutdev $audioout -midiindev $midiin -midioutdev $midiout $patch" ENTER
 
-sleep 2 # wait for pd to connect to alsa
+sleep 4 # wait for pd to connect to alsa
 
 # find the alsa device number of ttymidi
 echo "finding the alsa device number of ttymidi..."
@@ -81,11 +88,15 @@ echo "connecting pd's midi output to the arduino's midi input"
 aconnect $pdDev:1 $ttymidiDev:1
 
 ## arduino midi out -> pd midi in
-echo "connecting the arduino's midi output to pd's midi input"
-aconnect $ttymidiDev:0 $pdDev:0
+# TODO: need to use a different midi port for receiving midi, otherwise it loops itself for some reason
+#echo "connecting the arduino's midi output to pd's midi input"
+#aconnect $ttymidiDev:0 $pdDev:0
 
 # done!
 echo "All set!"
+echo "-------------------------------------------------------"
 
-
-
+# show us output of aconnect
+echo "output of `aconnect -lio`"
+aconnect -lio
+echo "-------------------------------------------------------"
